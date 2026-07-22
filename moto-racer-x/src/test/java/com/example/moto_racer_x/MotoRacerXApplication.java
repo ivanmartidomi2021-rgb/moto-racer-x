0d532e64ef7fbe5acd2a85153ed45c73c6c24176
ivanmartidomi2021-rgb <ivanmartidomi2021@gmail.com>
2026-07-22 08:13:45 -0600

    Add files via upload

---
+++ moto-racer-x/src/test/java/com/example/moto_racer_x/MotoRacerXApplication.java
@@ -0,0 +1,13 @@
+package com.example.moto_racer_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoRacerXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoRacerXApplication.class, args);
+    }
+
+}
